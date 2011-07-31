dbedf38692b55fb5fc3e9bfc4ac79002dbd8d40e
Richard Lapis <x.soldier@seznam.cz>
2011-07-31 14:54:20 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hosting::Application.config.secret_token = '81bbf05c462a39ec84a43ea37a66b2fe7b034ad8e97a658c5bc87d0432f179e7d11496f593c13103729bd6ad276a4e94ec07e897034f2dbfe0ccb792ebbadbd2'
